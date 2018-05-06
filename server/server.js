4e695bc140492411413f4afc2377671d255cfca5
unknown <kamil.nawrocki5@gmail.com>
2018-05-06 14:33:59 +0200

    Add tests for GET /users/me and POST /users

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 // Set for Heroku purpose - PORT is set while using heroku
@@ -103,6 +104,26 @@ app.patch('/todos/:id', (req, res) => {
 		res.status(400).send(e);
 	})
 })
+
+app.post('/users', (req, res) => {
+	var data = _.pick(req.body, ['email', 'password']);
+	var user = new User(data);
+
+	user.save().then(() => {
+		return user.generateAuthToken();
+	}).then((token) => {
+		// x- creates custom header
+		res.header('x-auth', token).send(user);
+	}).catch((e) => {
+		res.status(400).send(e);
+	})
+});
+
+// using middleware
+app.get('/users/me', authenticate, (req, res) => {
+	// this function body is next() in middleware
+	res.send(req.user);
+});
 
 app.listen(process.env.PORT, () => {
 	console.log(`Started on port ${process.env.PORT}`);
---
+++ server/tests/seed/seed.js
@@ -0,0 +1,47 @@
+const {ObjectID} = require('mongodb');
+const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+const jwt = require('jsonwebtoken');
+
+const validUserId = new ObjectID();
+const invalidUserId = new ObjectID();
+const users = [{
+	_id: validUserId,
+	email: 'validUser@example.com',
+	password: 'admin1',
+	tokens: [{
+		access: 'auth',
+		token: jwt.sign({_id: validUserId, access: 'auth'}, 'topFuckingSecretSecretKey').toString()
+	}]
+}, {
+	_id: invalidUserId,
+	email: 'invalidUser@example.com',
+	password: 'admin2',
+}]
+
+const todos = [{
+	_id: new ObjectID(),
+	text: 'First test todo',
+}, {
+	_id: new ObjectID(),
+	text: 'Second test todo',
+	completed: true,
+	completedAt: 666
+}];
+
+const populateTodos = (done) => {
+	Todo.remove({}).then(() => {
+		Todo.insertMany(todos);
+	}).then(() => done());
+};
+
+const populateUsers = (done) => {
+	User.remove({}).then(() => {
+		var userOne = new User(users[0]).save();
+		var userTwo = new User(users[1]).save();
+
+		return Promise.all([userOne, userTwo]);
+	}).then(() => done());
+}
+
+module.exports = {todos, populateTodos, users, populateUsers};
